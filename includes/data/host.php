aa65558ab5ea6d842f6854f8f96fee757539ed2c
nucc1 <nucc1@localhost>
2018-05-23 18:41:04 +0100

    dev

--- includes/data/host.php
+++ includes/data/host.php
@@ -17,7 +17,8 @@ class Data_Host
 	    var_dump($date);
 		// Calculate the last second of this day
 		$start_date = $date;
-		$end_date = gmmktime(23, 59, 59, date('M', $date), date('d', $date), date('Y', $date));
+
+		$end_date = mktime(23, 59, 59, date('M', $date), date('d', $date), date('Y', $date));
 
 		var_dump($end_date);
 
