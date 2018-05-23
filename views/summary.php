dff598b08020c0ceeddd6f6cb03cb847cbf3515f
nucc1 <nucc1@localhost>
2018-05-23 09:13:11 +0100

    handle cases of missing in or out bytes entries (no traffic in that direction)

--- views/summary.php
+++ views/summary.php
@@ -29,9 +29,9 @@ foreach ($this->data['data'] as $ip=>$row)
 			<tr data-in="', $row['bytes_in'], '" data-out="', $row['bytes_out'], '" data-total="', $row['bytes_total'], '">
 				<td><a href="', date('Y-m-d', $this->date), '/', $ip , '/">', $ip, '</a></td>
 				<td><a href="', date('Y-m-d', $this->date), '/', $ip , '/">', gethostbyaddr($ip), '</a></td>
-				<td>', Format::decimal_size($row['bytes_in']), '</td>
-				<td>', Format::decimal_size($row['bytes_out']), '</td>
-				<td>', Format::decimal_size($row['bytes_total']), '</td>
+				<td>', Format::decimal_size($row['bytes_in'] ?? 0), '</td>
+				<td>', Format::decimal_size($row['bytes_out'] ??0), '</td>
+				<td>', Format::decimal_size($row['bytes_total'] ?? 0), '</td>
 			</tr>';
 }
 ?>
