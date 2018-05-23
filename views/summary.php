0c41efb22c49ff62bac1fcab1e2e62cc89982b14
nucc1 <nucc1@localhost>
2018-05-23 09:08:47 +0100

    dev

--- views/summary.php
+++ views/summary.php
@@ -20,14 +20,14 @@
 		</tfoot>
 		<tbody>
 <?php
-foreach ($this->data['data'] as $row)
+foreach ($this->data['data'] as $ip=>$row)
 {
 
     var_dump($row);
 	echo '
 			<tr data-in="', $row['bytes_in'], '" data-out="', $row['bytes_out'], '" data-total="', $row['bytes_total'], '">
-				<td><a href="', date('Y-m-d', $this->date), '/', $row['ip'] , '/">', $row['ip'], '</a></td>
-				<td><a href="', date('Y-m-d', $this->date), '/', $row['ip'] , '/">', gethostbyaddr($row['ip']), '</a></td>
+				<td><a href="', date('Y-m-d', $this->date), '/', $ip , '/">', $ip, '</a></td>
+				<td><a href="', date('Y-m-d', $this->date), '/', $ip , '/">', gethostbyaddr($ip), '</a></td>
 				<td>', Format::decimal_size($row['bytes_in']), '</td>
 				<td>', Format::decimal_size($row['bytes_out']), '</td>
 				<td>', Format::decimal_size($row['bytes_total']), '</td>
