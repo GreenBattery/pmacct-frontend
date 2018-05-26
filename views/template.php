1631352395e6256c1b329ca7b450170a1a11400f
nucc1 <nucc1@localhost>
2018-05-26 23:27:41 +0100

    dev

--- views/template.php
+++ views/template.php
@@ -3,16 +3,16 @@
 <head>
 	<title>Data Usage Stats Viewer</title>
 	<base href="http://<?php echo $_SERVER['SERVER_NAME'] ?><?php echo dirname($_SERVER['PHP_SELF']) ?>/" />
-	<link rel="stylesheet" href="/css/normalize.css" />
-    <link rel="stylesheet" href="/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="/css/styles.css" />
+	<link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/styles.css" />
 </head>
 <body<?php if (!empty($this->page_id)) echo ' id="' . $this->page_id . '"'; ?>>
 	<?php echo $this->body ?>
 
-	<script src="/js/jquery-3.3.1.min.js"></script>
-	<script src="/js/highcharts.js"></script>
-	<script src="/js/scripts.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/highcharts.js"></script>
+	<script src="js/scripts.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
