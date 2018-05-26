f5a763cbf5d6f29a0de8869877403fb2232acff2
nucc1 <nucc1@localhost>
2018-05-26 23:33:30 +0100

    dev

--- views/template.php
+++ views/template.php
@@ -5,9 +5,29 @@
 	<base href="http://<?php echo $_SERVER['SERVER_NAME'] ?><?php echo dirname($_SERVER['PHP_SELF']) ?>/" />
 	<link rel="stylesheet" href="css/normalize.css" />
     <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
 	<link rel="stylesheet" href="css/styles.css" />
 </head>
 <body<?php if (!empty($this->page_id)) echo ' id="' . $this->page_id . '"'; ?>>
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <a class="navbar-brand" href="#">Pmacct Frontend</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="./">Home</a></li>
+                <li><a href="#about">Month</a></li>
+                <li><a href="#contact">Hosts</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+
 	<?php echo $this->body ?>
 
 	<script src="js/jquery-3.3.1.min.js"></script>
