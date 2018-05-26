19825ef201e4b32c504d2f4123fba764b2982de1
nucc1 <nucc1@localhost>
2018-05-26 23:50:06 +0100

    dev

--- views/host/day.php
+++ views/host/day.php
@@ -25,7 +25,7 @@ $this->page_id = 'host-day';
 foreach ($this->data['data'] as $hour=>$row)
 {
 	echo '
-		<tr data-in="', $row['bytes_in'], '" data-out="', $row['bytes_out'], '" data-total="', $row['bytes_in'] + $row['bytes_out'], '">
+		<tr data-in="', $row['bytes_in'] ?? 0 , '" data-out="', $row['bytes_out'] ?? 0, '" data-total="', $row['bytes_in'] ?? 0  + $row['bytes_out'] ?? 0, '">
 			<td>' . $hour . '</td>
 			<td>', Format::decimal_size($row['bytes_in']), '</td>
 			<td>', Format::decimal_size($row['bytes_out']), '</td>
