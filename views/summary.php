5e8a48b451e2d4fbb1aae68c0013285553a329df
nucc1 <nucc1@localhost>
2018-07-07 15:32:37 +0100

    handle new format of data in template

--- views/summary.php
+++ views/summary.php
@@ -16,9 +16,9 @@
 		<tfoot>
 			<tr>
 				<th colspan="2">Totals</th>
-				<td><?php echo Format::decimal_size($this->data['totals']['bytes_in']); ?></td>
-				<td><?php echo Format::decimal_size($this->data['totals']['bytes_out']); ?></td>
-				<td><?php echo Format::decimal_size($this->data['totals']['bytes_total']); ?></td>
+				<td><?php echo Format::decimal_size($this->data['totals']['in']); ?></td>
+				<td><?php echo Format::decimal_size($this->data['totals']['out']); ?></td>
+				<td><?php echo Format::decimal_size($this->data['totals']['in'] + $this->data['totals']['out']); ?></td>
 			</tr>
 		</tfoot>
 		<tbody>
