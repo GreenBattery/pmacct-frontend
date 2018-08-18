c8a4e01feed0d96bd1cf3cef6a83e0a6fa15c82e
nucc1 <nucc1@localhost>
2018-08-18 14:03:29 +0100

    dev

--- views/month.php
+++ views/month.php
@@ -1,6 +1,10 @@
 <?php
 $this->page_id = 'summary-month';
 var_dump($this->date);
+$dd = date_create("@{$this->date}");
+date_sub($dd, date_interval_create_from_date_string("1 month"))
+$lm = date_format($dd, "MM")
+var_dump($lm);
 ?>
 <h1>Statistics for <?php echo date('F Y', $this->date); ?></h1>
 <div>
