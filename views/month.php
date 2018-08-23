d5117c7f184e49c7a4704c5bd412107e5fac2e98
nucc1 <nucc1@localhost>
2018-08-23 19:12:47 +0100

    format bytes in human-friendly form in JS

--- views/month.php
+++ views/month.php
@@ -123,8 +123,9 @@ if ($nm < $cm) {
                 }
             ],
             'footerCallback': function(row, data, start, end, display) {
-                console.log( data);
-                var api = this.api()
+                var api = this.api();
+                console.log( api.column(2).footer().innerHTML);
+
                 $(api.column(2).footer()).html("here!")
             }
         }
