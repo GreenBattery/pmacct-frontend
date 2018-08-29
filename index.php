532d1c303b218a0eee913c8e39c021ed7559601d
nucc1 <nucc1@localhost>
2018-08-29 23:38:27 +0100

    default view displays stats for today

--- index.php
+++ index.php
@@ -7,15 +7,14 @@ require __DIR__ . '/includes/core.php';
 
 $tz = date_default_timezone_get(); //get timezone.
 
-//unix time for midnight on first day of the month (in UTC)
-$start_date = mktime(0,0,0, date('n'), 1, date('Y'));
+//unix time for midnight today
+
+$sd = strtotime("today");
+//var_dump($sd);
+$data = Data_Summary::day($sd);
 
 
-
-$data = Data_Summary::month($start_date);
-
-
-View::factory('month')
-    ->set('date', $start_date)
+View::factory('day')
+    ->set('date', $sd)
     ->set('data', $data)
     ->render();
