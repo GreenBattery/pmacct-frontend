020f5855a5837b77b28f9904ca21c0aedd11c818
nucc1 <nucc1@localhost>
2018-09-02 12:58:45 +0100

    handle zero values

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
-// credit to github gist: https://gist.github.com/trevershick/737205b0ba79d8877a43
+// based on github gist: https://gist.github.com/trevershick/737205b0ba79d8877a43
 function formatBytes(bytes) {
     if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
     var precision = 2;
 
+    if (bytes <= 0){
+        return "0 kB"
+    }
     var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB'],
         number = Math.floor(Math.log(bytes) / Math.log(1024));
+
+
     return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+
+
 }
