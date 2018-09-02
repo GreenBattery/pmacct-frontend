474e93316e15d114dfa865252fd4323c3826db9a
nucc1 <nucc1@localhost>
2018-09-02 12:59:11 +0100

    implement basic host view

--- host.php
+++ host.php
@@ -6,10 +6,22 @@
 
 require __DIR__ . '/includes/core.php';
 date_default_timezone_set(Config::$tz);
-// get date and IP from query string if available.
-$date = strtotime($_GET['date']) ?? strtotime("today"); //make a unix timestamp of it.
+
+$date = strtotime("today");
+//if date was supplied use it.
+if (array_key_exists("date", $_GET)) {
+    $date = strtotime($_GET["date"]);
+}
+
+//var_dump($date);
+
+$ip = "";
 //if no IP supplied, should show all hosts
-$ip = $_GET['ip']; //should validate this?
+if(array_key_exists("ip", $_GET)) {
+    $ip = $_GET['ip']; //should validate this?
+} else {
+    header("Location: ./day.php");
+}
 
 $data = Data_Host::day($ip, $date);
 
@@ -18,4 +30,3 @@ View::factory('host/day')
 	->set('date', $date)
 	->set('data', $data)
 	->render();
-?>
--- views/host/day.php
+++ views/host/day.php
@@ -1,9 +1,10 @@
 <?php
 $this->page_id = 'host-day';
+//var_dump($this->data);
 ?>
 <h1>Statistics for <?= $this->ip; ?>  on <?= date('M d, Y', $this->date); ?></h1>
 
-<table id="host">
+<table  id="host-summary">
 	<thead>
 		<tr>
 			<th>Time</th>
@@ -15,9 +16,9 @@ $this->page_id = 'host-day';
 	<tfoot>
 		<tr>
 			<th>Totals</th>
-			<td><?php echo Format::decimal_size($this->data['totals']['bytes_in']); ?></td>
-			<td><?php echo Format::decimal_size($this->data['totals']['bytes_out']); ?></td>
-			<td><?php echo Format::decimal_size($this->data['totals']['bytes_total']); ?></td>
+			<td><?php echo $this->data['totals']['bytes_in']; ?></td>
+			<td><?php echo $this->data['totals']['bytes_out']; ?></td>
+			<td><?php echo $this->data['totals']['bytes_total']; ?></td>
 		</tr>
 	</tfoot>
 	<tbody>
@@ -26,18 +27,75 @@ foreach ($this->data['data'] as $hour=>$row)
 {
 	echo '<tr data-in="', $row['bytes_in'] ?? 0 , '" data-out="', $row['bytes_out'] ?? 0 , '" data-total="', ($row['bytes_in'] ?? 0 )  + ($row['bytes_out'] ?? 0), '">
 			<td>' . $hour . '</td>
-			<td>', Format::decimal_size($row['bytes_in'] ?? 0 ), '</td>
-			<td>', Format::decimal_size($row['bytes_out'] ?? 0), '</td>
-			<td>', Format::decimal_size($row['bytes_total'] ?? 0), '</td>
-
-		<tr data-in="', $row['bytes_in'] ?? 0 , '" data-out="', $row['bytes_out'] ?? 0, '" data-total="', $row['bytes_in'] ?? 0  + $row['bytes_out'] ?? 0, '">
-			<td>' . $hour . '</td>
-			<td>', Format::decimal_size($row['bytes_in'] ?? 0), '</td>
-			<td>', Format::decimal_size($row['bytes_out'] ?? 0 ), '</td>
-			<td>', Format::decimal_size($row['bytes_total'] ?? 0), '</td>
-
+			<td>', $row['bytes_in'] ?? 0 , '</td>
+			<td>', $row['bytes_out'] ?? 0, '</td>
+			<td>', $row['bytes_total'] ?? 0, '</td>
 		</tr>';
 }
 ?>
 	</tbody>
 </table>
+
+<script>
+    $(function() {
+        var opts = {
+            lengthMenu: [[-1], ["All"]],
+            columns: [
+                {data: 'Hour'},
+                {
+                    data: 'in',
+                    render: function(data, type, row) {
+                        if (type === "display") {
+                            return formatBytes(data);
+                        }else {
+                            return data;
+                        }
+
+                    }
+                },
+                {
+                    data: 'out',
+                    render: function(data, type, row) {
+                        if (type === "display") {
+                            return formatBytes(data);
+                        }else {
+                            return data;
+                        }
+
+                    }
+                },
+                {
+                    data: 'total',
+                    render: function(data, type, row) {
+                        if (type === "display") {
+                            return formatBytes(data);
+                        }else {
+                            return data;
+                        }
+
+                    }
+                }
+            ],
+            'footerCallback': function(row, data, start, end, display) {
+                var api = this.api();
+                var inTotal = api.column(1).footer().innerText;
+                if ($.isNumeric(inTotal)) {
+                    $(api.column(1).footer()).html(formatBytes(inTotal));
+                }
+
+
+                var outTotal = api.column(2).footer().innerText;
+                if ($.isNumeric(outTotal)) {
+                    $(api.column(2).footer()).html(formatBytes(outTotal));
+                }
+
+
+                var sumTotal = api.column(3).footer().innerText;
+                if ($.isNumeric(sumTotal)) {
+                    $(api.column(3).footer()).html(formatBytes(sumTotal));
+                }
+            }
+        }
+        $('#host-summary').DataTable(opts);
+    })
+</script>
