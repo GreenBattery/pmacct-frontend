7dce0ed803c1ddcf6e85b9e2659eb6f486ee0ebe
nucc1 <nucc1@localhost>
2018-11-18 20:51:23 +0000

    include schema for cache table

--- docs/schema.sql
+++ docs/schema.sql
@@ -42,3 +42,16 @@ create table outbound_%m%Y (
         PRIMARY KEY (agent_id, mac_src, mac_dst, vlan, ip_src, ip_dst, src_port, dst_port, ip_proto, stamp_inserted)
 );-----------------------------------------
 
+-- Table structure for main statistics cache/summary.
+CREATE TABLE `main_summary` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `ip` varchar(40) NOT NULL DEFAULT '0',
+  `duration_type` varchar(10) NOT NULL DEFAULT '0',
+  `duration` varchar(12) NOT NULL DEFAULT '0',
+  `bytes_in` bigint(20) unsigned DEFAULT '0',
+  `bytes_out` bigint(20) unsigned DEFAULT '0',
+  `stamp_inserted` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip_duration_type_duration` (`ip`,`duration_type`,`duration`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='duration type specifies whether this is a state for day, month or year. \r\n\r\nthe duration field specifies which unit of the duration the stat covers. Example, for a duration_type = day, then the duration field will contain "2018-10-15" meaning the stats are for the 15th of October 2018. For duration_type = month, you should expect "102018" meaning october 2018.'
+
